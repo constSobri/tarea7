b53e2087fafe6701ba055b3d2f534e35d3f21279
ang <57077187+constSobri@users.noreply.github.com>
2023-03-02 18:24:17 -0600

    fechas

---
+++ fechas.js
@@ -0,0 +1,11 @@
+const fechaHoy = new Date()
+
+const naciEn = new Date(2000, 02, 26)
+
+const masTarde = fechaHoy > naciEn
+
+let dia = naciEn.getDate()
+let mes = naciEn.getMonth()
+let anyo = naciEn.getFullYear()
+
+console.log(anyo)
